2632e11bc3b95c36fec50918f2227f2847bceb89
Sunmeet Singh <renu.kabeer@gmail.com>
2022-10-20 22:50:32 +0530

    changes

--- src/Components/NewAndNotable.jsx
+++ src/Components/NewAndNotable.jsx
@@ -11,6 +11,9 @@ export const NewAndNotable = () => {
             <NotableCard subImgSrc={"https://i.seadn.io/gcs/files/c00d3dd212a1b3b9a2340cb15e81a880.png?auto=format&w=128"} mainImgSrc={"https://i.seadn.io/gcs/files/a54a001eb5a78877b6e3647896db262b.jpg?auto=format&w=1920"} tagLine={"Anthony Hopkins - The Eternal"}/>
             <NotableCard subImgSrc={"https://i.seadn.io/gcs/files/c00d3dd212a1b3b9a2340cb15e81a880.png?auto=format&w=128"} mainImgSrc={"https://i.seadn.io/gcs/files/a54a001eb5a78877b6e3647896db262b.jpg?auto=format&w=1920"} tagLine={"Anthony Hopkins - The Eternal"}/>
             <NotableCard subImgSrc={"https://i.seadn.io/gcs/files/c00d3dd212a1b3b9a2340cb15e81a880.png?auto=format&w=128"} mainImgSrc={"https://i.seadn.io/gcs/files/a54a001eb5a78877b6e3647896db262b.jpg?auto=format&w=1920"} tagLine={"Anthony Hopkins - The Eternal"}/>
+            <NotableCard subImgSrc={"https://i.seadn.io/gcs/files/c00d3dd212a1b3b9a2340cb15e81a880.png?auto=format&w=128"} mainImgSrc={"https://i.seadn.io/gcs/files/a54a001eb5a78877b6e3647896db262b.jpg?auto=format&w=1920"} tagLine={"Anthony Hopkins - The Eternal"}/>
+            <NotableCard subImgSrc={"https://i.seadn.io/gcs/files/c00d3dd212a1b3b9a2340cb15e81a880.png?auto=format&w=128"} mainImgSrc={"https://i.seadn.io/gcs/files/a54a001eb5a78877b6e3647896db262b.jpg?auto=format&w=1920"} tagLine={"Anthony Hopkins - The Eternal"}/>
+
         </div>
     </div>
   )
